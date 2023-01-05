9c26be8be8687e2b4e0b16bf2adca83c84cef56b
vixbc <92274245+vixbc@users.noreply.github.com>
2023-01-05 19:27:37 +0000

    initial local storage test
    checking the local storage feature with basic checkbox options

---
+++ js/profile.js
@@ -0,0 +1,45 @@
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+  }
+  else {
+    // Too bad, no localStorage for us
+  }
+  
+
+  console.log(storageAvailable('sessionStorage'))
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name="color"]:checked');
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    localStorage.setItem('person', values);;
+}); 
+
