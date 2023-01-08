35267d2ccaf6b81d4ac1b3de5ed7c4f16cc4df48
vixbc <92274245+vixbc@users.noreply.github.com>
2023-01-08 21:15:23 +0000

    updating logged data
    checking that the required data is being pulled correctly using the displayWeather function

--- js/profile.js
+++ js/profile.js
@@ -62,10 +62,12 @@ let userLocation = {
                 selectedArray = responseArray[i]
                 let lat = [selectedArray.latitude]
                 let lon = [selectedArray.longitude]
+                let timeZone = [selectedArray.timezone]
                 let locName = [selectedArray.name+", "+selectedArray.country_code]
                 localStorage.setItem("lat", lat)
                 localStorage.setItem("lon", lon)
                 localStorage.setItem("placeName",locName)
+                localStorage.setItem("timezone",timeZone)
                 
             }
         }
@@ -83,22 +85,26 @@ let userLocation = {
 
 }
 
+
+//defining the variables which take data from client storage
 let userLat = localStorage.getItem("lat")
 let userLon = localStorage.getItem("lon")
 let userLocName = localStorage.getItem("placeName")
+let userTimeZone = localStorage.getItem("timezone")
 
 
-
+//weather api call
 let weather = {
     fetchWeather: function () {
-        fetch("https://api.open-meteo.com/v1/forecast?latitude=" +userLat + "&longitude=" +userLon + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature&daily=temperature_2m_max,temperature_2m_min,windspeed_10m_max&current_weather=true&timezone=Europe%2FLondon"
+        fetch("https://api.open-meteo.com/v1/forecast?latitude=" +userLat + "&longitude=" +userLon + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone="+userTimeZone
         ).then((response) => response.json()).then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
-        const { name } = data;
-        const {icon, description} = data.weather;
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        console.log(name, icon,description,temp,humidity,speed)
+        var name = userLocName;
+        const {temperature,time,windspeed} = data.current_weather;
+        const {temperature_2m_max,temperature_2m_min,precipitation_sum} = data.daily;
+        
+        console.log(name,temperature,time.slice(11,16),windspeed,temperature_2m_max[0],temperature_2m_min[0],precipitation_sum[0])
+        console.log(data)
     }
 }
