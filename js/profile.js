a961c23164b1a706a972576bfbcf4d5efad5d029
vixbc <92274245+vixbc@users.noreply.github.com>
2023-01-08 20:16:38 +0000

    location & weather api
    created initial calls for data from weather & location api. stores location info to client storage, uses client side location info to produce weather results

--- js/profile.js
+++ js/profile.js
@@ -43,3 +43,54 @@ btn.addEventListener('click', (event) => {
     localStorage.setItem('person', values);;
 }); 
 
+
+
+
+
+
+
+
+
+// weather & location APIs
+let userLocation = {
+    // case insensitive filteration of the api city search response to yield single city in given country.
+    countryFilter: function (userCountry, responseArray) {
+        let selectedArray = {};
+        for (let i = 0; i < responseArray.length; i++) {
+            compResponse = responseArray[i].country
+            if (compResponse.toUpperCase() == userCountry.toUpperCase()) {
+                selectedArray = responseArray[i]
+                let lat = [selectedArray.latitude]
+                let lon = [selectedArray.longitude]
+                localStorage.setItem("lat", lat)
+                localStorage.setItem("lon", lon)
+            }
+        }
+    },
+    // fetching city info from api paired with country filtering
+    fetchLocation: function (userCity, userCountry) {
+        fetch(
+            "https://geocoding-api.open-meteo.com/v1/search?name=" + userCity).then((response) => response.json()).then((data) => (this.countryFilter(userCountry, data.results)
+            ))
+
+    },
+    displayLocation: function (data) {
+        const { name } = data;
+    },
+
+}
+
+let userLat = localStorage.getItem("lat")
+let userLon = localStorage.getItem("lon")
+
+
+
+let weather = {
+    fetchWeather: function () {
+        fetch("https://api.open-meteo.com/v1/forecast?latitude=" +userLat + "&longitude=" +userLon + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature&daily=temperature_2m_max,temperature_2m_min,windspeed_10m_max&current_weather=true&timezone=Europe%2FLondon"
+        ).then((response) => response.json()).then((data) => console.log(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+    }
+}
