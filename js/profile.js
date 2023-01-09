e4e8f4c768ea3c173401fe1e4cac315c57d12c20
vixbc <92274245+vixbc@users.noreply.github.com>
2023-01-09 17:53:21 +0000

    dynamic check boxes
    added options list to the js and eventlistners to dynamically create them, save the results

--- js/profile.js
+++ js/profile.js
@@ -25,27 +25,103 @@ function storageAvailable(type) {
 
 if (storageAvailable('localStorage')) {
     // Yippee! We can use localStorage awesomeness
-  }
-  else {
+}
+else {
     // Too bad, no localStorage for us
-  }
-  
+}
 
-  console.log(storageAvailable('sessionStorage'))
 
-const btn = document.querySelector('#btn');
+console.log(storageAvailable('sessionStorage'))
+
+
+// health preferences list
+let healthPreferences =
+    { 'Alcohol-Cocktail': 'alcohol-cocktail', 'Alcohol-Free': 'alcohol-free', 'Celery-Free': 'celery-free', 'Crustcean-Free': 'crustacean-free', 'Dairy-Free': 'dairy-free', 'DASH': 'DASH', 'Egg-Free': 'egg-free', 'Fish-Free': 'fish-free', 'FODMAP-Free': 'fodmap-free', 'Gluten-Free': 'gluten-free', 'Immuno-Supportive': 'immuno-supportive', 'Keto-Friendly': 'keto-friendly', 'Kidney-Friendly': 'kidney-friendly', 'Kosher': 'kosher', 'Low Potassium': 'low-potassium', 'Low Sugar': 'low-sugar', 'Lupine-Free': 'lupine-free', 'Mediterranean': 'Mediterranean', 'Mollusk-Free': 'mollusk-free', 'Mustard-Free': 'mustard-free', 'No oil added': 'No-oil-added', 'Paleo': 'paleo', 'Peanut-Free': 'peanut-free', 'Pescatarian': 'pecatarian', 'Pork-Free': 'pork-free', 'Red-Meat-Free': 'red-meat-free', 'Sesame-Free': 'sesame-free', 'Shellfish-Free': 'shellfish-free', 'Soy-Free': 'soy-free', 'Sugar-Conscious': 'sugar-conscious', 'Sulfite-Free': 'sulfite-free', 'Tree-Nut-Free': 'tree-nut-free', 'Vegan': 'vegan', 'Vegetarian': 'vegetarian', 'Wheat-Free': 'wheat-free', }
+
+// diet preferences list
+let dietPreferences =
+    { 'Balanced': 'balanced', 'High-Fiber': 'high-fiber', 'High-Protein': 'high-protein', 'Low-Carb': 'low-carb', 'Low-Fat': 'low-fat', 'Low-Sodium': 'low-sodium', }
+
+
+// dynamically creating checkboxes once page loads
+// diet
+$(document).ready(function () {
+    $('#edit-diet').ready(function () {
+        var list = ['High-Fiber', 'High-Protein', 'Balanced', 'Low-Carb', 'Low-Fat', 'Low-Sodium'];
+        for (let value of list) {
+            $('#diet-container')
+                .append(`<input type="checkbox" id="${value}" name="diet" class"preference" value="${dietPreferences[value]}">`)
+                .append(`<label for="${value}">${value}</label></div>`);
+        }
+
+    })
+});
+
+//health
+$(document).ready(function () {
+    $('#edit-diet').ready(function () {
+        var list = ['Alcohol-Cocktail','Alcohol-Free','Celery-Free','Crustcean-Free','Dairy-Free','DASH','Egg-Free','Fish-Free','FODMAP-Free','Gluten-Free','Immuno-Supportive','Keto-Friendly','Kidney-Friendly','Kosher','Low Potassium','Low Sugar','Lupine-Free','Mediterranean','Mollusk-Free','Mustard-Free','No oil added','Paleo','Peanut-Free','Pescatarian','Pork-Free','Red-Meat-Free','Sesame-Free','Shellfish-Free','Soy-Free','Sugar-Conscious','Sulfite-Free','Tree-Nut-Free','Vegan','Vegetarian','Wheat-Free',];
+        for (let value of list) {
+            $('#health-container')
+                .append(`<input type="checkbox" id="${value}" name="health" class"preference" value="${healthPreferences[value]}">`)
+                .append(`<label for="${value}">${value}</label></div>`);
+        }
+
+    })
+});
+
+//cuisine
+
+$(document).ready(function () {
+    $('.diet-preferences').ready(function () {
+        var list = ['american','asian','british','caribbean','central europe','chinese','eastern europe','french','greek','indian','italian','japanese','korean','kosher','mediterranean','mexican','middle eastern','nordic','south american','south east asian','world',];
+        for (let value of list) {
+            $('#cuisine-container')
+                .append(`<input type="checkbox" id="${value}" name="cuisine" class"preference" value="${value}">`)
+                .append(`<label for="${value}">${value}</label></div>`);
+        }
+
+    })
+});
+
+
+
+
+const btn = document.querySelector('#save-diet');
+// diet save
 btn.addEventListener('click', (event) => {
-    let checkboxes = document.querySelectorAll('input[name="color"]:checked');
-    let values = [];
+    let checkboxes = document.querySelectorAll('input[name="diet"]:checked');
+    let values = "";
     checkboxes.forEach((checkbox) => {
-        values.push(checkbox.value);
+        values += checkbox.value;
     });
-    localStorage.setItem('person', values);;
-}); 
+    localStorage.setItem('diet', values);
+});
+
+// health save
+btn.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name="health"]:checked');
+    let values = "";
+    checkboxes.forEach((checkbox) => {
+        values += checkbox.value;
+    });
+    localStorage.setItem('health', values);
+});
+
+// cuisine save
+btn.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name="cuisine"]:checked');
+    let values = "";
+    checkboxes.forEach((checkbox) => {
+        values += checkbox.value;
+    });
+    localStorage.setItem('cuisine', values);
+});
 
 
 
 
+// https://api.edamam.com/api/recipes/v2?type=public&app_id=5c1f4878&app_key=d220cee0e567234581f36d1404f5f7b3&diet=high-fiber&diet=high-protein&diet=low-carb&diet=low-fat&health=alcohol-cocktail&health=alcohol-free&health=celery-free&health=crustacean-free&health=dairy-free&health=DASH&health=egg-free&health=fish-free&health=fodmap-free&health=gluten-free&health=immuno-supportive&health=keto-friendly&health=kidney-friendly&cuisineType=American&cuisineType=Asian&cuisineType=British
 
 
 
@@ -63,12 +139,12 @@ let userLocation = {
                 let lat = [selectedArray.latitude]
                 let lon = [selectedArray.longitude]
                 let timeZone = [selectedArray.timezone]
-                let locName = [selectedArray.name+", "+selectedArray.country_code]
+                let locName = [selectedArray.name + ", " + selectedArray.country_code]
                 localStorage.setItem("lat", lat)
                 localStorage.setItem("lon", lon)
-                localStorage.setItem("placeName",locName)
-                localStorage.setItem("timezone",timeZone)
-                
+                localStorage.setItem("placeName", locName)
+                localStorage.setItem("timezone", timeZone)
+
             }
         }
     },
@@ -82,9 +158,9 @@ let userLocation = {
     displayLocation: function (data) {
         const { name } = data;
     },
-    search: function(){
+    search: function () {
         this.fetchLocation(
-            document.querySelector(".city-bar").value,document.querySelector(".country-bar").value)
+            document.querySelector(".city-bar").value, document.querySelector(".country-bar").value)
     }
 
 }
@@ -99,78 +175,78 @@ let userTimeZone = localStorage.getItem("timezone")
 
 // weather code descriptions
 let weatherCodes = {
-0:"Clear sky",
-1:"Mainly clear",
-2:"Partly cloudy",
-3:"Overcast",
-45:"Fog",
-48:"Depositing rime fog",
-51:"Light drizzle",
-53:"Moderate drizzle",
-55:"Dense drizzle",
-56:"Light, freezing drizzle",
-57:"Dense, freezing drizzle",
-61:"Slight rain",
-63:"Moderate rain", 
-65:"Heavy rain",
-66:"Light, freezing drizzle", 
-67:"Heavy, freezing drizzle",
-71:"Slight snowfall", 
-73:"Moderate snowfall", 
-75:"Heavy snowfall",
-77:"Snow grains",
-80:"Slight rain",
-81:"Moderate rain", 
-82:"Violent rain",
-85:"Slight snow showers", 
-86:"Heavy snow showers",
-95:"Slight to moderate thunderstorm",
-96:"Thunderstorm with slight hail", 
-99: "Thunderstorm with heavy hail"
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    56: "Light, freezing drizzle",
+    57: "Dense, freezing drizzle",
+    61: "Slight rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    66: "Light, freezing drizzle",
+    67: "Heavy, freezing drizzle",
+    71: "Slight snowfall",
+    73: "Moderate snowfall",
+    75: "Heavy snowfall",
+    77: "Snow grains",
+    80: "Slight rain",
+    81: "Moderate rain",
+    82: "Violent rain",
+    85: "Slight snow showers",
+    86: "Heavy snow showers",
+    95: "Slight to moderate thunderstorm",
+    96: "Thunderstorm with slight hail",
+    99: "Thunderstorm with heavy hail"
 }
 
 //weather api call
 let weather = {
     fetchWeather: function () {
-        if (userLat&&userLon&&userTimeZone != null){
-        fetch("https://api.open-meteo.com/v1/forecast?latitude=" +userLat + "&longitude=" +userLon + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone="+userTimeZone
-        ).then((response) => response.json()).then((data) => this.displayWeather(data));
-    }
-        else{}
-    
-},
-    displayWeather: function (data) {
-        const {temperature,time,windspeed,weathercode} = data.current_weather;
-        const {temperature_2m_max,temperature_2m_min,precipitation_sum} = data.daily;
-        
-        console.log(time.slice(11,16),temperature,windspeed,temperature_2m_max[0],temperature_2m_min[0],precipitation_sum[0]);
-        console.log(data);
-        document.querySelector(".city").innerHTML="weather in:\n"+userLocName;
-        document.querySelector(".weatherNow").innerHTML="weather now:\n"+weatherCodes[weathercode];
-        document.querySelector(".temp").innerHTML="temp now:\n"+temperature+"°C";
-        document.querySelector(".maxTemp").innerHTML="max temp today:\n"+temperature_2m_max[0]+"°C";
-        document.querySelector(".minTemp").innerHTML="min temp today:\n"+temperature_2m_min[0]+"°C";
-        document.querySelector(".precipitation").innerHTML="total rain today:\n"+precipitation_sum[0]+"mm";
-        document.querySelector(".wind").innerHTML="max windspeed:\n"+windspeed+"km/h";
+        if (userLat && userLon && userTimeZone != null) {
+            fetch("https://api.open-meteo.com/v1/forecast?latitude=" + userLat + "&longitude=" + userLon + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone=" + userTimeZone
+            ).then((response) => response.json()).then((data) => this.displayWeather(data));
+        }
+        else { }
+
     },
-    
+    displayWeather: function (data) {
+        const { temperature, time, windspeed, weathercode } = data.current_weather;
+        const { temperature_2m_max, temperature_2m_min, precipitation_sum } = data.daily;
+
+        console.log(time.slice(11, 16), temperature, windspeed, temperature_2m_max[0], temperature_2m_min[0], precipitation_sum[0]);
+        console.log(data);
+        document.querySelector(".city").innerHTML = "weather in:\n" + userLocName;
+        document.querySelector(".weatherNow").innerHTML = "weather now:\n" + weatherCodes[weathercode];
+        document.querySelector(".temp").innerHTML = "temp now:\n" + temperature + "°C";
+        document.querySelector(".maxTemp").innerHTML = "max temp today:\n" + temperature_2m_max[0] + "°C";
+        document.querySelector(".minTemp").innerHTML = "min temp today:\n" + temperature_2m_min[0] + "°C";
+        document.querySelector(".precipitation").innerHTML = "total rain today:\n" + precipitation_sum[0] + "mm";
+        document.querySelector(".wind").innerHTML = "max windspeed:\n" + windspeed + "km/h";
+    },
+
 }
 
 
-
-window.onload = function (){
+// automatically kicks off the fetch weather function so the info is populated. if there is no stored data, a message is shown to direct user to the update location button
+window.onload = function () {
     weather.fetchWeather();
 }
 
-document.querySelector(".search button").addEventListener("click",function(){
+document.querySelector(".search button").addEventListener("click", function () {
     userLocation.search();
 })
 
-document.querySelector(".search button").addEventListener("click",function(){
-    document.querySelector(".resultList").innerHTML="Search result:"
+document.querySelector(".search button").addEventListener("click", function () {
+    document.querySelector(".resultList").innerHTML = "Search result:"
 })
 
-document.querySelector(".save").addEventListener("click", function(){
+document.querySelector(".save").addEventListener("click", function () {
     location.reload()
 })
 
@@ -178,4 +254,7 @@ document.querySelector(".save").addEventListener("click", function(){
 // timer to refresh the page 
 setTimeout(() => {
     document.location.reload();
-  }, 900000);
+}, 900000);
+
+
+
