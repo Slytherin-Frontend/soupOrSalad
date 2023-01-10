93bf328d835d32533162d4df4a7bb17c2818a9cf
CrystalPharma <crystalpharmauk@gmail.com>
2023-01-10 22:31:38 +0000

    randomised results

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ let healthQuery = localStorage.getItem('healthQuery')|| ''
 let dietQuery = localStorage.getItem('dietQuery')|| ''
 let choiceQuery = localStorage.getItem('choiceQuery')||''
 // let choice =localStorage.getItem('choice');
- var queryURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${inputText}&app_id=${id}&app_key=${key}&from=0&to=20${dietQuery}${healthQuery}${cusineQuery}${choiceQuery}&imageSize=REGULAR`;
+var queryURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${inputText}&app_id=${id}&app_key=${key}&from=0&to=20${dietQuery}${healthQuery}${cusineQuery}${choiceQuery}&imageSize=REGULAR`;
+
+
 
 
  $.ajax({
@@ -33,93 +35,56 @@ let choiceQuery = localStorage.getItem('choiceQuery')||''
     console.log(response)
     // console.log(response.hits[0].recipe.image);
     console.log(response)
-    let childNode = `<div class="card-group"><div class="card">
-<img class="card-img-top" src="${response.hits[0].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title">${response.hits[0].recipe.label}</h5>
-    <div class="text-center" id="meal-type">Meal type: ${response.hits[0].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[0].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[0].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[0].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[0].recipe.url}">More
-        details</a>
-</div>
-</div>
-<div class="card">
-<img class="card-img-top" src="${response.hits[1].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title" id="salad5">${response.hits[1].recipe.label}</h5>
-    <div class="text-center" id="meal-type">Meal type: ${response.hits[1].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[1].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[1].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[1].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[1].recipe.url}">More
-        details</a>
-</div>
-</div>
-<div class="card">
-<img class="card-img-top" src="${response.hits[2].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title" id="salad5">${response.hits[2].recipe.label}</h5>
-    <div class="text-center" id="meal-type"><b>Meal type: </b>${response.hits[2].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[2].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[2].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[2].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[2].recipe.url}">More
-    details</a>
-</div>
-</div>
-</div>
-<div class="card-group"><div class="card">
-<img class="card-img-top" src="${response.hits[3].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title" id="salad5">${response.hits[3].recipe.label}</h5>
-    <div class="text-center" id="meal-type">Meal type: ${response.hits[3].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[3].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[3].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[3].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[3].recipe.url}">More
-    details</a>
-</div>
-</div>
-<div class="card">
-<img class="card-img-top" src="${response.hits[9].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title" id="salad5">${response.hits[9].recipe.label}</h5>
-    <div class="text-center" id="meal-type">Meal type: ${response.hits[9].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[9].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[9].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[9].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[9].recipe.url}">More
-    details</a>
-</div>
-</div>
-<div class="card">
-<img class="card-img-top" src="${response.hits[5].recipe.image}"
-    alt="Card image cap">
-<div class="card-body">
-    <h5 class="card-title" id="salad5">${response.hits[5].recipe.label}</h5>
-    <div class="text-center" id="meal-type">Meal type: ${response.hits[5].recipe.mealType}</div>
-    <div class ="text-center" id="diet-labels"> ${response.hits[5].recipe.dietLabels}</div>
-    <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[5].recipe.ingredientLines}</div>
-    <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[5].recipe.totalTime} mins</div>
-    <br>
-    <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[5].recipe.url}">More
-    details</a>
-</div>
-</div>
-</div>`
+
+    var arr0_19 = [...Array(20).keys()];
+    shuffle(arr0_19);
+    
+    var gpCards = ["", ""]
+    var tmpStr = ""
+    var newIdx = 0
+    for (let j=0; j<6; j++){
+        newIdx = arr0_19[j];
+        tmpStr = `<div class="card">
+        <img class="card-img-top" src="${response.hits[newIdx].recipe.image}"
+            alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${response.hits[newIdx].recipe.label}</h5>
+            <div class="text-center" id="meal-type">Meal type: ${response.hits[newIdx].recipe.mealType}</div>
+            <div class ="text-center" id="diet-labels"> ${response.hits[newIdx].recipe.dietLabels}</div>
+            <div class ="text-center" id="ingredients" style="font-size: 0.9rem;"><h6>Ingredients:</h6> ${response.hits[newIdx].recipe.ingredientLines}</div>
+            <div class="text-center" id="time"><b>Preparation time: </b>${response.hits[newIdx].recipe.totalTime} mins</div>
+            <br>
+            <a type="button" class="btn btn-secondary" target="_blank" a href="${response.hits[newIdx].recipe.url}">More
+                details</a>
+        </div>
+        </div>`
+        if (j%2 === 0) 
+            gpCards[0] += tmpStr;
+        else 
+            gpCards[1] += tmpStr;
+        
+    }
+    let childNode = '<div class="card-group">'+gpCards[0]+
+        '</div><div class="card-group">'+gpCards[1]+'</div>'
     document.querySelector('#recipe-container').innerHTML = childNode;
     // recipeContainer.html(childNode);
     });
 });
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
