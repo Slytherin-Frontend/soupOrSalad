71ed0dd338f0d0ab6f10265d723baa87c652597e
vixbc <92274245+vixbc@users.noreply.github.com>
2023-01-12 18:18:27 +0000

    Delete recipes.js

--- js/recipes.js
+++
@@ -1,5 +0,0 @@
-url = https://api.edamam.com/api/recipes/v2?type=public&app_id=d68014e5&app_key=71d418ddd224b48e3eccdc64a90aa982&q=soup&random=true
-
-var app_id= "d68014e5";
-var app_key= "71d418ddd224b48e3eccdc64a90aa982";
-
